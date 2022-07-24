89e84147d11f03c50edeccfc4d17612c7509bd09
summer <yuanjie0311@dingtalk.com>
2022-07-25 00:10:01 +0800

    feat: 🚀 新增postcss-pxtorem插件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -40,6 +40,7 @@ const babelRuntimeEntryHelpers = require.resolve("@babel/runtime/helpers/esm/ass
 const babelRuntimeRegenerator = require.resolve("@babel/runtime/regenerator", {
 	paths: [babelRuntimeEntry]
 });
+const pxtorem = require("postcss-pxtorem");
 
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
 // makes for a smoother build process.
@@ -133,30 +134,35 @@ module.exports = function (webpackEnv) {
 											stage: 3
 										}
 									],
-									[
-										"postcss-px-to-viewport",
-										{
-											unitToConvert: "px", //需要转换的单位，默认为"px"
-											viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度
-											//   viewportHeight: 1334, //视窗的高度，根据375设备的宽度来指定，一般指定667，也可以不配置
-											unitPrecision: 5, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-											propList: ["*"], // 能转化为vw的属性列表
-											viewportUnit: "vw", // 指定需要转换成的视窗单位，建议使用vw
-											fontViewportUnit: "vw", //字体使用的视口单位
-											selectorBlackList: [".ignore-", ".hairlines"], //指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-											minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-											mediaQuery: false, // 允许在媒体查询中转换`px`
-											replace: true, //是否直接更换属性值，而不添加备用属性
-											exclude: [/RightBar/, /gotop.vue/], //忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
-											landscape: false, //是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
-											landscapeUnit: "vw", //横屏时使用的单位
-											landscapeWidth: 1134 //横屏时使用的视口宽度
-										}
-									],
+									// [
+									// 	"postcss-px-to-viewport",
+									// 	{
+									// 		unitToConvert: "px", //需要转换的单位，默认为"px"
+									// 		viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度
+									// 		//   viewportHeight: 1334, //视窗的高度，根据375设备的宽度来指定，一般指定667，也可以不配置
+									// 		unitPrecision: 5, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+									// 		propList: ["*"], // 能转化为vw的属性列表
+									// 		viewportUnit: "vw", // 指定需要转换成的视窗单位，建议使用vw
+									// 		fontViewportUnit: "vw", //字体使用的视口单位
+									// 		selectorBlackList: [".ignore-", ".hairlines"], //指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+									// 		minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+									// 		mediaQuery: false, // 允许在媒体查询中转换`px`
+									// 		replace: true, //是否直接更换属性值，而不添加备用属性
+									// 		exclude: [/RightBar/, /gotop.vue/], //忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+									// 		landscape: false, //是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
+									// 		landscapeUnit: "vw", //横屏时使用的单位
+									// 		landscapeWidth: 1134 //横屏时使用的视口宽度
+									// 	}
+									// ],
 									// Adds PostCSS Normalize as the reset css with default options,
 									// so that it honors browserslist config in package.json
 									// which in turn let's users customize the target behavior as per their needs.
-									"postcss-normalize"
+									"postcss-normalize",
+									pxtorem({
+										rootValue: 75,
+										propList: ["*"],
+										exclude: /node_modules/i
+									})
 							  ]
 							: [
 									"tailwindcss",
@@ -170,6 +176,14 @@ module.exports = function (webpackEnv) {
 											stage: 3
 										}
 									]
+									// [
+									// 	"postcss-pxtorem",
+									// 	{
+									// 		rootValue: 100,
+									// 		propsList: ["*"],
+									// 		exclude: /node_modules/i
+									// 	}
+									// ]
 							  ]
 					},
 					sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment
---
+++ src/components/Loadable.tsx
@@ -0,0 +1,16 @@
+import Loadable from "react-loadable";
+// import { Icon } from "antd-mobile-icons";
+
+export default (loader: any, render?: any) => {
+	// if (!render) {
+	// 	return Loadable({
+	// 		loader,
+	// 		loading: () => <Icon type="loading" className="loading" size="lg" />
+	// 	});
+	// }
+	// return Loadable({
+	// 	loader,
+	// 	loading: () => <Icon type="loading" className="loading" size="lg" />,
+	// 	render
+	// });
+};
