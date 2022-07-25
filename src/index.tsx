a4d8c5936864ad7a3bc6df2bdbd312094b588404
袁杰 <yuanjie0311@dingtalk.com>
2022-07-25 15:52:48 +0800

    docs: 📚 结构调整

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "lib-flexible/flexible";
-import App from "./App";
+
+import { HashRouter } from "react-router-dom";
+import Router from "@/router/index";
 
 import "@/styles/reset.scss";
 import "@/global.scss";
@@ -9,6 +11,8 @@ import "@/global.scss";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<HashRouter>
+			<Router />
+		</HashRouter>
 	</React.StrictMode>
 );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Route, HashRouter, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import routers from "./config";
 
 const rootRouter: any = [...routers];
 
 const Router = () => {
 	const routes = useRoutes(rootRouter);
-	console.log(routes, "routes");
 	return routes;
 };
 
